9f5ffbe9083ebdbc9b88f72a286bf78cc323787c
Julien <julien.delbosc@epita.fr>
2020-04-17 16:50:49 +0200

    Synchronisation of UI and Stats made

--- Assets/Scripts/Photon/AvatarSetup.cs
+++ Assets/Scripts/Photon/AvatarSetup.cs
@@ -10,11 +10,33 @@ public class AvatarSetup : MonoBehaviour
     public int charVal;
     public GameObject myChar;
 
+    public float maxH;
+    public float speed;
+
     private void Awake()
     {
         PV = GetComponent<PhotonView>();
         if (PV.IsMine)
             PV.RPC("RPC_AddChar", RpcTarget.AllBuffered, PlayerInfos.PI.mySelectedChar);
+        
+        switch (charVal)
+        {
+            case 0:
+                maxH = 100;
+                speed = 30;
+                break;
+            case 1:
+                maxH = 200;
+                speed = 30;
+                break;
+            case 2:
+                maxH = 300;
+                speed = 10;
+                break;
+            default:
+                maxH = 100;
+                break;
+        }
     }
 
     [PunRPC]
--- Assets/Scripts/playerStats.cs
+++ Assets/Scripts/playerStats.cs
@@ -8,52 +8,33 @@ public class playerStats : MonoBehaviour
 {
     public AvatarSetup AS;
     
-    public float maxH;
     public float currentH;
     public Slider healthBar;
 
     [SerializeField]
-    private int speed;
+    private float speed;
 
     [SerializeField]
     public int coinAmount;
     public TMP_Text coinHeap;
-
-
-    private PlayerManager _player;
-
+    
     public void Awake()
-    {
+    { 
         AS = gameObject.GetComponent<AvatarSetup>();
+    }
 
+    public void Start()
+    {
+        currentH = AS.maxH;
+        speed = AS.speed;
+        
         healthBar = GameObject.Find("Canvas").transform.GetChild(0).GetComponent<Slider>();
-        coinHeap = GameObject.Find("Canvas").transform.GetChild(2).GetComponent<TMP_Text>();
-
-        switch (AS.charVal)
-        {
-            case 0:
-                maxH = 100;
-                speed = 30;
-                break;
-            case 1:
-                maxH = 200;
-                speed = 30;
-                break;
-            case 2:
-                maxH = 300;
-                speed = 10;
-                break;
-            default:
-                maxH = 100;
-                break;
-        }
-
-        currentH = maxH;
-
+        coinHeap = GameObject.Find("Canvas").transform.GetChild(0).GetChild(4).GetComponent<TMP_Text>();
     }
 
     public void Update()
     {
-        _player.Health = currentH;
+        healthBar.value = currentH / AS.maxH;
+        coinHeap.text = coinAmount.ToString();
     }
 }
