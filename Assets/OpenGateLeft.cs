e30fea831a143420ef05289e37e9e1259ee98ed3
Sirnowe <nono3000@outlook.fr>
2020-04-21 12:04:33 +0200

    debut stagger

---
+++ Assets/OpenGateLeft.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGateLeft : MonoBehaviour
+{
+    [SerializeField] private bool Isopen;
+    
+    [SerializeField]private SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = transform.parent.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Isopen)
+        {
+            sr.enabled = false;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (Isopen && col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.transform.Translate(-6,0,0);
+        }
+    }
+}
---
+++ Assets/OpenGateTop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGateTop : MonoBehaviour
+{
+   [SerializeField] private bool Isopen;
+   
+
+   [SerializeField]private SpriteRenderer sr;
+   private void Start()
+   {
+      sr = transform.parent.GetComponent<SpriteRenderer>();
+   }
+   private void Update()
+   {
+      if (Isopen)
+      {
+         sr.enabled = false;
+      }
+   }
+
+   private void OnCollisionEnter2D(Collision2D col)
+   {
+      if (Isopen && col.gameObject.CompareTag("Player"))
+      {
+         col.gameObject.transform.Translate(0,5.5f,0);
+      }
+   }
+}
