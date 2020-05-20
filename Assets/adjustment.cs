f515c372fab48f2c332c9ac8320f5dcfb3442f36
Julien <julien.delbosc@epita.fr>
2020-05-21 00:49:20 +0200

    beginning of dungeon fix, have 2 finish to fix the camera movement, finishing all this shit 2morrow

---
+++ Assets/adjustment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class adjustment : MonoBehaviour
+{
+    private void Start()
+    {
+        int c = 0;
+        foreach (var p in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            Debug.Log(c+ "_Found " + p.name);
+            c++;
+        }
+    }
+}
