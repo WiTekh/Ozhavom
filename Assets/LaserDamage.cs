269c11a94209f7811162d31eccf375af3170ebc2
Alexandre Netchaev <alexandre.nechaev@gmail.com>
2020-05-21 22:50:33 +0200

    stp marche

---
+++ Assets/LaserDamage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDamage : MonoBehaviour
+{
+    public int dmg;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("YES");
+        if (collision.CompareTag("Ennemy"))
+        {
+            Debug.Log($"Dealt {dmg}");
+            collision.gameObject.GetComponent<ennemyStats>().health -= dmg;
+        }
+    }
+}
