20bb23a24db988dbb146acb478a5bf09b0a376cc
Julien <julien.delbosc@epita.fr>
2020-05-22 20:09:01 +0200

    final artworks

--- Assets/Scripts/Movement/WeaponShoot.cs
+++ Assets/Scripts/Movement/WeaponShoot.cs
@@ -3,71 +3,52 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeaponShoot : MonoBehaviour
 {
     private AvatarSetup AS;
     private PhotonView PV;
-    
-    public int dmg;
 
     [SerializeField] GameObject bulletprefab;
 
-    private PlayerMovement move;
-    private LineRenderer laser;
-    public Transform LaserHit;
-
     [SerializeField] private int firerate;
 
-    private float fire;
+    public TMP_Text coinsAmount;
+    public Slider healthBar;
 
-    // Update is called once per frame
+    [SerializeField] int fire;
+
     private void Start()
     {
         Transform avatar = transform.parent.parent.parent;
         AS = avatar.GetComponent<AvatarSetup>();
         PV = avatar.GetComponent<PhotonView>();
 
-        move = transform.parent.parent.parent.GetComponent<PlayerMovement>();
+        healthBar = GameSetup.GS.healthBar;
+        coinsAmount = GameSetup.GS.coinsAmount;
+
         fire = firerate;
-        laser = GetComponent<LineRenderer>();
-        laser.enabled = false;
-        laser.useWorldSpace = true;
     }
 
     void Update()
-    { 
+    {
         if (PV.IsMine)
         {
-            if (fire == 20)
-            {
-                move.enabled = true;
-                laser.enabled = false;
-            }
-        
             if (Input.GetMouseButton(0) && firerate <= fire)
             {
-                Debug.Log("LASER");
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right);
-                LaserHit.position = hit.point;
-                if (hit.collider.gameObject.tag == "Ennemy")
-                {
-                    Debug.Log($"Dealt {dmg}");
-                    hit.collider.gameObject.GetComponent<ennemyStats>().health -= dmg;
-                }
-                laser.sortingLayerName = "Player";
-                laser.SetPosition(0,transform.position);
-                laser.SetPosition(1,LaserHit.position);
-                laser.enabled = true;
-                move.enabled = false;
+                GameObject bullet = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "Bullet"),
+                    transform.position, transform.rotation);
+                bullet.GetComponent<BulletColision>().dmg = 50;
                 fire = 0;
             }
-
-            else if (fire<firerate)
+            else if (fire < firerate)
             {
-                fire+=0.25f;
+                fire++;
             }
         }
+
     }
 }
