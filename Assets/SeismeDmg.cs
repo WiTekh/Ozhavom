d34652cc8da3d0ec53e26f95e532f3d545b5e666
Sirnowe <nono3000@outlook.fr>
2020-05-25 21:02:18 +0200

    forsure

---
+++ Assets/SeismeDmg.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeismeDmg : MonoBehaviour
+{
+    [SerializeField] private float dmg;
+    [SerializeField] private SpriteRenderer sr;
+
+    private int time = 0;
+    // Start is called before the first frame update
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ennemy")&& time == 10)
+        {
+            other.gameObject.GetComponent<ennemyStats>().health -= dmg;
+        }
+
+       
+    }
+
+    private void Update()
+    {
+        
+        if (time == 31)
+        {
+            Destroy(gameObject);
+        }
+
+        if (time %10 >= 5)
+        {
+            sr.enabled = true;
+        }
+        else
+        {
+            sr.enabled = false;
+        }
+        time++;
+    }
+}
---
+++ Assets/blocked.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blocked : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("BadBullet"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
