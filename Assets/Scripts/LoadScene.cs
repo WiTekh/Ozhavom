a2c209c1980279b2604fb14facb663a58c943d95
Julien <julien.delbosc@epita.fr>
2020-05-25 11:48:55 +0200

    2e Etage - Debut

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,45 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void LoadJrMenu()
+    {
+        SceneManager.LoadScene(4);
+    }
+    
+    public void LoadCrMenu()
+    {
+        SceneManager.LoadScene(3);
+    }
+    
+    public void LoadSolo()
+    {
+        GameObject.Find("AMBIANCE").transform.GetChild(1).GetChild(1).GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene(8);
+    }
+    
+    public void LoadRoomSystemMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadRandomRoom()
+    {
+        GameObject.Find("AMBIANCE").transform.GetChild(1).GetChild(0).GetComponent<AudioSource>().Play();
+        SceneManager.LoadScene(6);
+    }
+
+    public void LoadMainMenu()//Loading main menu and disconnecting the player so it can be reconnected when joining it
+    {
+        SceneManager.LoadScene(0);
+        PhotonNetwork.Disconnect();
+    }
+
+    public void QuitGame()//Quit Game
+    {
+        PhotonNetwork.Disconnect();
+        Application.Quit();
+    }
+}
